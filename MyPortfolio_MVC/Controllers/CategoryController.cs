fbd530f3a2c271e1a9c13fe8b89b5285d8190c3b
mfkahraman <mfurkankahraman@gmail.com>
2024-11-15 19:51:03 +0300

    Add project files.

---
+++ MyPortfolio_MVC/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using MyPortfolio_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio_MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        MyPortfolioEntities db = new MyPortfolioEntities();
+
+        public ActionResult Index()
+        {
+            var values = db.Categories.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCategory(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
