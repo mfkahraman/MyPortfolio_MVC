026d38743e48f1e3c9af9f69c2f1cf054d192281
mfkahraman <mfurkankahraman@gmail.com>
2024-11-20 23:44:59 +0300

    Default Controller Eklendi. luther tema eklendi

--- MyPortfolio_MVC/Controllers/EducationController.cs
+++ MyPortfolio_MVC/Controllers/EducationController.cs
@@ -24,5 +24,41 @@ namespace MyPortfolio_MVC.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public ActionResult CreateEducation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateEducation(Education entity)
+        {
+            db.Educations.Add(entity);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index)); //nameof(Index) ile "Index" aynı işlemi yapıyor
+        }
+
+        [HttpGet]
+        public ActionResult UpdateEducation(int id)
+        {
+            var entity = db.Educations.Find(id);
+            return View(entity);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateEducation(Education model)
+        {
+            var tbl = db.Educations.Find(model.Id);
+            tbl.SchoolName = model.SchoolName;
+            tbl.Description = model.Description;
+            tbl.StartDate = model.StartDate;
+            tbl.EndDate = model.EndDate;
+            tbl.Degree = model.Degree;
+            tbl.Department = model.Department;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
